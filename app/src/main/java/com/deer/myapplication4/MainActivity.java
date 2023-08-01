771cde1fe03d6d06d93a6d5c57886c587249595e
jinzhu <jinzhu@cece.com>
2023-08-01 21:03:27 +0800

    3333

--- app/src/main/java/com/deer/myapplication4/MainActivity.java
+++ app/src/main/java/com/deer/myapplication4/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
          init();
+//         initName();
     }
     private void init(){
        Log.i("tag","main分支");
