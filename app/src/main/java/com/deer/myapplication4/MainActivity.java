ef6338ac56a6b7d5bf45095f31c3bfaf3511159f
jinzhu <jinzhu@cece.com>
2023-08-01 21:10:56 +0800

    666

--- app/src/main/java/com/deer/myapplication4/MainActivity.java
+++ app/src/main/java/com/deer/myapplication4/MainActivity.java
@@ -17,4 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private void init(){
        Log.i("tag","lallal");
     }
+    private void init21(){
+        Log.i("tag","lallal");
+    }
 }
