2b18a5872996ebdb958d1671efd7f88a7c56d10a
jinzhu <jinzhu@cece.com>
2023-08-01 20:55:25 +0800

    1111

--- app/src/main/java/com/deer/myapplication4/MainActivity.java
+++ app/src/main/java/com/deer/myapplication4/MainActivity.java
@@ -1,10 +1,8 @@
 package com.deer.myapplication4;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
